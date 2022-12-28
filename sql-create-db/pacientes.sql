5c104ee8bfe2d569fb4e7b19ea58ef87d01de830
Francisco sandoval <fesv1975@gmail.com>
2022-12-27 23:07:01 -0300

    data inicial

--- sql-create-db/pacientes.sql
+++ sql-create-db/pacientes.sql
@@ -5,3 +5,10 @@ CREATE TABLE public.pacientes (
     edad integer NOT NULL,
     PRIMARY KEY(id)
 );
+
+INSERT INTO public.pacientes (nombre,apellidos,edad) VALUES
+	 ('Kevin','Cid Arenas',30),
+	 ('Jorge','Mendoza Garrido',37),
+	 ('Edmundo','Garrido Miranda',18),
+	 ('Sebastian (modificado)','Reyes Zapata',60);
+
